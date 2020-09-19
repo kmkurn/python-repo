e4d36dfebc20659bc6017f74d836adbe06aca902
Kemal Kurniawan <kemal@kkurniawan.com>
2020-09-19 21:01:10 +1000

    Use black instead of yapf

--- pre-commit.sh
+++ pre-commit.sh
@@ -4,6 +4,6 @@
 PYTHON_FILES=$(git diff --cached --name-only --diff-filter=d | grep -E '\.py')
 if [ -n "$PYTHON_FILES" ]; then
     for file in $PYTHON_FILES; do
-        yapf -i "$file" && git add "$file"
+        black "$file" && git add "$file"
     done
 fi
